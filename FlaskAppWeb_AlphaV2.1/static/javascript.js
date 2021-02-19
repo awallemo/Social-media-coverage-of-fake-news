553ba2485b1229cdc62ac279f119a40a4cc83de2
awallemo <awallemo_98@hotmail.com>
2021-02-19 17:12:00 +0100

    linechart

--- FlaskAppWeb_AlphaV2.1/static/javascript.js
+++ FlaskAppWeb_AlphaV2.1/static/javascript.js
@@ -1,6 +1,9 @@
 //Creating global variable that stores the dictionary pulled from the Twitter API storing all of the data from the tweets.
 var allData;
 var allDates = [];
+var newalldates  =[];
+//Sett en tweet/retweet til value og datoen(tiden) til key.
+var amountdict = {};
 
 //EventListener that calls the function start() when the site is loaded
 window.addEventListener('DOMContentLoaded', () => start(), false);
@@ -15,6 +18,7 @@ async function start() {
         console.log(list);
         created_at();
         chart(list);
+        linechart(newalldates);
 
     }, false);
 };
@@ -27,6 +31,19 @@ async function fetchdata() {
             allData = data.data
         });
 };
+
+//Dividing the data before putting it into the linechart.
+
+/**function divideData(allData){
+    //sett tid til key og tweeten til value.
+    for (let i = 0; i < allData.length; i++){
+        amountdict[allData[i]["created_at"]] = allData[i]["id"]
+    }
+    sorteddata = allData.sort(function(a,b){
+        return b.created_at-a.created_at
+    })
+    
+}**/
 
 //showing/hiding bar-DIV
 function barCanvas() {
@@ -85,6 +102,51 @@ function chart(allData) {
         }
     });
 };
+//plotting linechart of the tweets that were created. 
+function linechart(allDates) {
+    //divideData(allData)
+    
+    for (let i = 0; i < allDates.length; i++){
+    var ctx = document.getElementById('LineChart').getContext('2d');
+    var myLineChart = new Chart(ctx, {
+        type: 'line',
+        options: {
+            scales: {
+              xAxes: [{
+                type: 'time'
+              }]
+            }
+          },
+          data: {
+            labels: [allDates[i]],
+            datasets: [{
+              label: 'Demo',
+              data: [{
+                  t: allDates[i],
+                  y: i
+                }],
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+            }]
+          }
+        });
+    }
+}
 
 // Extracting the public metrics from allData
 // The API returns 100 tweets that can be in the format for a orignal tweet, a reply to a tweet, a quote of a tweet or a retweet of a tweet
@@ -140,19 +202,21 @@ function findAmountOfTweets() {
 }
 
 
-
-//plotting linechart of the tweets that were created. 
-function linechart() {
-    console.log(allDates)
-
-}
-
 function created_at() {
+    
     for (let i = 0; i < allData.length; i++) {
         const element = allData[i]["created_at"];
         allDates.push(element)
-        console.log()
+        
     }
+    for (let i = 0; i < allDates.length; i++){
+        allDates[i] = allDates[i].replace("T", " ")
+        allDates[i] = allDates[i].replace(".000Z", "")
+        console.log(allDates[i])
+        newalldates.push(new Date(allDates[i])) 
+    }
+    
+    console.log("ALLDATES " + newalldates)
 }
 
 // a function call of that calls on the api
